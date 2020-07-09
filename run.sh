f32c51025715a8ddd3b6e5401466cabbb3a5b467
Rohan Mandal <66948695+golemcli@users.noreply.github.com>
2020-07-09 22:15:12 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,17 @@ EOFSS
 echo "Creating Virtual Display..."
 Xvfb :51 & export DISPLAY=:51
 echo "Virtual display created :)"
-./Otohits/otohits-app
+./Otohits/otohits-app &
+
+while [ 1 != 0 ]
+do
+while [[ ! $(pidof otohits-app) ]]; do
+        echo "Restarting...."
+        killall otohits-app otohits-viewer chromium-browser chrome-sandbox
+        ./Otohits/otohits-app
+        exit
+        echo "Restarted...."
+        sleep 120
+done
+sleep 120
+done
